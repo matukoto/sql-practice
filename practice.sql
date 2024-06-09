1d0d7a677ea4a0f1bc03280a6277d7892cc22aea
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-06-09 21:22:44 +0900

    feat: 問題2のSQLを追加

--- practice.sql
+++ practice.sql
@@ -8,3 +8,20 @@ WHERE
   lvl = 2
 ORDER BY
   total_amt DESC;
+
+-- 問題2
+/**
+都道府県別 入院率を算出
+入院率の高い順
+**/
+select
+  p.pf_code as 都道府県コード,
+  p.pf_name as 都道府県名,
+  (
+    h.inp_yes / (h.inp_yes + h.inp_no + h.unidentified)
+  ) * 100 as 入院率
+from
+  prefecture p
+  inner join hospitalization h
+order by
+  入院率;
